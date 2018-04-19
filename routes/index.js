124914bb736e4b64d509b865909f46618e06f8db
Marc Pomar <marc@faable.com>
2018-04-19 11:42:44 +0200

    nodemail example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
 
+const sendAwesomeMail = require('../mail/sendMail');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', { messages: req.flash('info') });
 });
+
+
+/* SEND A MAIL */
+router.post('/send-email', (req, res, next) => {
+  console.log("RECEIVED MAIL");
+  console.log(req.body);
+  sendAwesomeMail('boyander@gmail.com',{mail_title:"HOLA CARACOLA"})
+  .then(() => {
+    req.flash('info', 'MENSAJE ENVIADO');
+    res.redirect('/')
+  })
+  .catch(error => {
+    req.flash('info', 'ERROR, NO SE HA PODIDO ENVIAR EL MENSAJE');
+    next(error)
+  })
+});
+
 
 module.exports = router;
